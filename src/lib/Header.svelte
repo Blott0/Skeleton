a6b2d6c897b7bc935df7677ca83207e58cbd12fc
Rik Meijer <rikmeijer442@gmail.com>
2023-03-22 13:29:36 +0100

    first commit

---
+++ src/lib/Header.svelte
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<header>
+    <div class="container">
+        <a href="/" class="brand">Home</a>
+        <input id="openmenu" type="checkbox">
+        <label aria-label="toggle menu" for="openmenu" class="screen">
+        </label>
+        <nav>
+            <ul>
+                <li><a href="/page1">something</a></li>
+                <li><a href="/page1">something else</a></li>
+                <li><a href="/page1">another thing</a></li>
+                <li><a href="/page1">thing</a></li>
+            </ul>
+        </nav>
+        <label aria-label="toggle menu" for="openmenu" class="hamburger">
+            <span />
+            <span />
+            <span />
+        </label>
+    </div>
+</header>
+
+<style>
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+
+</script>
+
+    <section>
+        <div class="container">
+            <h1>Header 1</h1>
+            <h2>Header 2</h2>
+            <h3>Header 3</h3>
+            <h4>Header 4</h4>
+            <p>Lorem ipsum dolor sit amet consectetur <a href="/">adipisicing elit</a> . Esse ipsa a velit, et exercitationem praesentium qui quis ab, voluptatibus ipsum assumenda necessitatibus aliquam? Hic blanditiis dolorum vel magni asperiores facere.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse ipsa a velit, et exercitationem praesentium qui quis ab, voluptatibus ipsum assumenda necessitatibus aliquam? Hic blanditiis dolorum vel magni asperiores facere.</p>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <form action="/">
+                <fieldset>
+                    <legend>Form legend</legend>
+                    <label for="input1">label for input<input id="input1" type="text"></label>
+                    <label for="input2">label for input<input id="input2" type="text"></label>
+                    <label for="radio1">label for radio1<input id="radio1" value="1" name="radio" type="radio"></label>
+                    <label for="radio2">label for radio2<input id="radio2" value="2" name="radio" type="radio"></label>
+                    <label for="radio3">label for radio3<input id="radio3" value="3" name="radio" type="radio"></label>
+                    <label for="textarea">label for input
+                        <textarea name="textarea" id="textarea"></textarea>
+                    </label>
+                    <label for="select">label for select
+                        <select name="select" id="select">
+                            <option value="10">10</option>
+                            <option value="orange">Orange</option>
+                            <option value="volvo">Volvo</option>
+                            <option value="cat">Cat</option>
+                        </select>
+                    </label>
+                    <label for="submit">label for submit<input id="submit" type="submit"></label>
+                </fieldset>
+            </form>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <h1>Header 1</h1>
+            <h2>Header 2</h2>
+            <h3>Header 3</h3>
+            <h4>Header 4</h4>
+            <p>Lorem ipsum dolor sit amet consectetur <a href="/">adipisicing elit</a> . Esse ipsa a velit, et exercitationem praesentium qui quis ab, voluptatibus ipsum assumenda necessitatibus aliquam? Hic blanditiis dolorum vel magni asperiores facere.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse ipsa a velit, et exercitationem praesentium qui quis ab, voluptatibus ipsum assumenda necessitatibus aliquam? Hic blanditiis dolorum vel magni asperiores facere.</p>
+        </div>
+    </section>
+
+<style>
+
+</style>
