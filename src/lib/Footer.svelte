43b4a0329d9e3922d0c1c91bab608d32e6287f9a
Rik Meijer <rikmeijer442@gmail.com>
2023-03-23 11:45:01 +0100

    added footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -3,11 +3,26 @@
 </script>
 
 <footer>
-    <div class="container">
-        
+    <div class="main-footer">
+        <div class="container">
+            some info here
+        </div>
+    </div>
+    <div class="under-footer">
+        <div class="container">
+            door: rik meijer
+        </div>
     </div>
 </footer>
 
 <style>
-
+    .main-footer > .container {
+        height: 30vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .under-footer {
+        background-color: greenyellow;
+    }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,3 +15,5 @@
 <main>
     <slot />
 </main>
+
+<Footer />
