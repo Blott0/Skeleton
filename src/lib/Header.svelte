581d1dd36e21f3fb61c6eb36202e0d9f223038fd
Rik Meijer <rikmeijer442@gmail.com>
2023-03-24 15:07:50 +0100

    menu resets after use

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,43 +2,46 @@
 
     export let scrollY
 
+    let menuopen
+    let submenu = {}
+
 </script>
 
 <header class:top="{!scrollY}">
     <div class="container">
-        <a href="/" class="brand">Home</a>
-        <input id="openmenu" type="checkbox">
+        <a on:click="{()=>{menuopen = !menuopen}}" href="/" class="brand">Home</a>
+        <input bind:checked="{menuopen}" id="openmenu" type="checkbox">
         <label aria-label="toggle menu" for="openmenu" class="screen">
         </label>
         <nav>
             <ul>
-                <li><a href="/page1">something</a></li>
-                <li><a href="/page1">something else</a></li>
+                <li><a on:click="{()=>{menuopen = !menuopen}}" href="/page1">something</a></li>
+                <li><a on:click="{()=>{menuopen = !menuopen}}" href="/page1">something else</a></li>
                 <li><label for="submenu1">submenu</label>
-                    <input id="submenu1" type="checkbox">
+                    <input bind:checked="{submenu[0]}" id="submenu1" type="checkbox">
                     <ul>
                         <li><label for="submenu1">Back</label></li>
-                        <li><a href="/">submenu item</a></li>
-                        <li><a href="/">submenu item2</a></li>
-                        <li><a href="/">submenu item2</a></li>
-                        <li><a href="/">submenu item4</a></li>
+                        <li><a on:click="{()=>{menuopen = !menuopen;submenu = {}}}" href="/">submenu item</a></li>
+                        <li><a on:click="{()=>{menuopen = !menuopen;submenu = {}}}" href="/">submenu item2</a></li>
+                        <li><a on:click="{()=>{menuopen = !menuopen;submenu = {}}}" href="/">submenu item2</a></li>
+                        <li><a on:click="{()=>{menuopen = !menuopen;submenu = {}}}" href="/">submenu item4</a></li>
                     </ul>
                 </li>
-                <li><a href="/page1">another thing</a></li>
+                <li><a on:click="{()=>{menuopen = !menuopen}}" href="/page1">another thing</a></li>
                 <li><label for="submenu2">submenu2</label>
-                    <input id="submenu2" type="checkbox">
+                    <input bind:checked="{submenu[1]}" id="submenu2" type="checkbox">
                     <ul>
                         <li><label for="submenu2">Back</label></li>
-                        <li><a href="/">submenu item</a></li>
-                        <li><a href="/">submenu item2</a></li>
-                        <li><a href="/">submenu item2</a></li>
-                        <li><a href="/">submenu item4</a></li>
+                        <li><a on:click="{()=>{menuopen = !menuopen;submenu = {}}}" href="/">submenu item</a></li>
+                        <li><a on:click="{()=>{menuopen = !menuopen;submenu = {}}}" href="/">submenu item2</a></li>
+                        <li><a on:click="{()=>{menuopen = !menuopen;submenu = {}}}" href="/">submenu item2</a></li>
+                        <li><a on:click="{()=>{menuopen = !menuopen;submenu = {}}}" href="/">submenu item4</a></li>
                     </ul>
                 </li>
-                <li><a href="/page1">thing</a></li>
+                <li><a on:click="{()=>{menuopen = !menuopen}}" href="/page1">thing</a></li>
             </ul>
         </nav>
-        <label aria-label="toggle menu" for="openmenu" class="hamburger">
+        <label on:click="{()=>{submenu = {}}}" aria-label="toggle menu" for="openmenu" class="hamburger">
             <span />
             <span />
             <span />
